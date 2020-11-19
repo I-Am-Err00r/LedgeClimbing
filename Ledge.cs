3b719220279159fff4ce73f3cd06c538dfaf3256
I_Am_Err00r <40243263+I-Am-Err00r@users.noreply.github.com>
2020-11-19 15:18:47 -0600

    Update Ledge.cs

--- Ledge.cs
+++ Ledge.cs
@@ -1,8 +1,9 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Ledge : MonoBehaviour
 {
-
+    public float hangingHorizontalOffset;
+    public float hangingVerticalOffset;
 }
